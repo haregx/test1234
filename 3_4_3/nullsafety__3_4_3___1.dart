92b7a285ae4c82cf1e0acc0dd8bbfc03063ff9d2
Hary Reger <hreger@hareg.com>
2025-08-20 16:25:25 +0200

    3.4.3

--- 3_4_3/nullsafety__3_4_3___1.dart
+++ 3_4_3/nullsafety__3_4_3___1.dart
@@ -10,16 +10,38 @@ void main() {
 
   // Erstelle leere Liste für Temperaturen
   List<double?> temps = [];
-  
+  List<double?> rain = [];
+  List<double?> wind = [];
+
   // Füge die Temperaturen der Wetterdaten in die Liste ein
   temps.add(weatherData[0]['temp'] ?? 0);
   temps.add(weatherData[1]['temp'] ?? 0);
   temps.add(weatherData[2]['temp'] ?? 0);
 
+  // Füge die Niederschlagswerte der Wetterdaten in die Liste ein
+  rain.add(weatherData[0]['rain'] ?? 0);
+  rain.add(weatherData[1]['rain'] ?? 0);
+  rain.add(weatherData[2]['rain'] ?? 0);
+
+  // Füge die Windgeschwindigkeiten der Wetterdaten in die Liste ein
+  wind.add(weatherData[0]['wind'] ?? 0);
+  wind.add(weatherData[1]['wind'] ?? 0);
+  wind.add(weatherData[2]['wind'] ?? 0);
+
   int validTempCount = 0;
   validTempCount += weatherData[0]['temp'] != null ? 1 : 0;
   validTempCount += weatherData[1]['temp'] != null ? 1 : 0;
   validTempCount += weatherData[2]['temp'] != null ? 1 : 0;
+
+  int validRainCount = 0;
+  validRainCount += weatherData[0]['rain'] != null ? 1 : 0;
+  validRainCount += weatherData[1]['rain'] != null ? 1 : 0;
+  validRainCount += weatherData[2]['rain'] != null ? 1 : 0;
+
+  int validWindCount = 0;
+  validWindCount += weatherData[0]['wind'] != null ? 1 : 0;
+  validWindCount += weatherData[1]['wind'] != null ? 1 : 0;
+  validWindCount += weatherData[2]['wind'] != null ? 1 : 0;
 
   double avgTemp = ((temps[0] ?? 0) + (temps[1] ?? 0) + (temps[2] ?? 0));
   if (validTempCount == 0) {
@@ -27,6 +49,24 @@ void main() {
   } else {
     avgTemp /= validTempCount;
     print('Durchschnittstemperatur: ${avgTemp.toStringAsFixed(1)} °C');
-    print('Es lagen für ${weatherData.length - validTempCount} von ${weatherData.length} Tage keine Werte vor.');
+    print('Es lagen für ${weatherData.length - validTempCount} von ${weatherData.length} Tage keine Werte für die Temperatur vor.');
+  }
+
+  double avgRain = ((rain[0] ?? 0) + (rain[1] ?? 0) + (rain[2] ?? 0));
+  if (validRainCount == 0) {
+    print('Es lagen keinerlei gültigen Niederschlagswerte vor.');
+  } else {
+    avgRain /= validRainCount;
+    print('Durchschnittlicher Niederschlag: ${avgRain.toStringAsFixed(1)} mm');
+    print('Es lagen für ${weatherData.length - validRainCount} von ${weatherData.length} Tage keine Werte für den Niederschlag vor.');
+  }
+
+  double avgWind = ((wind[0] ?? 0) + (wind[1] ?? 0) + (wind[2] ?? 0));
+  if (validWindCount == 0) {
+    print('Es lagen keinerlei gültigen Windwerte vor.');
+  } else {
+    avgWind /= validWindCount;
+    print('Durchschnittliche Windgeschwindigkeit: ${avgWind.toStringAsFixed(1)} km/h');
+    print('Es lagen für ${weatherData.length - validWindCount} von ${weatherData.length} Tage keine Werte für den Wind vor.');
   }
 }
