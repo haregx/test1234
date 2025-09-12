8a9fc901adf0fdf80ea0bee65b1fc68e618772d0
Harry Reger <hreger@hareg.com>
2025-09-12 09:44:37 +0200

    Project

--- Project/Flutter/app_1/lib/main.dart
+++ Project/Flutter/app_1/lib/main.dart
@@ -20,13 +20,30 @@ class MainApp extends StatelessWidget {
             padding:  EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
               children: [
                 SizedBox(height: 20, child: Text("height: 20"),),
-                Container(
-                  height: 100,
-              //    padding: EdgeInsets.all(10),
-              //    margin: EdgeInsets.all(10),
-                  child: Text('Container'),
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withAlpha(80),
+                        blurRadius: 10,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Container(
+                    height: 100,
+                    //    padding: EdgeInsets.all(10),
+                    //    margin: EdgeInsets.all(10),
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text('Container')
+                    ),
+                  ),
                 ),
                 Text('This is not my first Flutter app.'),
                 Text('But I am still learning Flutter.'),
